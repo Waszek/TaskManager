eeae48a96e243b1cfc5618ba28fe79fc05a77106
Krzysztof W <waszur20@gmail.com>
2026-07-28 21:11:14 +0200

    added ViewSets in views and router in urls and also serializer

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def perform_create(self, serializer):
+        print(self.request.user)
+        serializer.save(owner=self.request.user)
+        
