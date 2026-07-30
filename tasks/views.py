4f86f117df3f78ca42c0bf305411528d4679e9c8
Krzysztof W <waszur20@gmail.com>
2026-07-30 20:03:44 +0200

    added permission and modify get on queryset

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
+   
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         print(self.request.user)
